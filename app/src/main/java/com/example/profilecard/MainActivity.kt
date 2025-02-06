c36fd34b0d5b858a21108a18b7004ef97dca9d55
96SLM <slmitchell2@pstcc.edu>
2025-02-06 12:05:35 -0500

    Created Composable function 'ProfileBio' that takes the Parameters 'image''name''bio' current error involves OneDrive conflict

--- app/src/main/java/com/example/profilecard/MainActivity.kt
+++ app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.profilecard.ui.theme.ProfileCardTheme
 
@@ -19,29 +19,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProfileCardTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+//                    /*Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )*/
+//                }
             }
         }
     }
 }
 //Pick up form "Run your first app on the Android Emulator"
-@Composable
+/*@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+}*/
+
+@Composable
+fun ProfileCardApp(){}
+
+@Composable
+fun ProfileBio(image: Painter, name: String, bio: String){
+    Column{
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = name
+        )
+        Text(
+            text = bio
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ProfileCardPreview() {
     ProfileCardTheme {
-        Greeting("Android")
+        ProfileCardApp()
+        ProfileBio(
+            image = painterResource(id = R.drawable.rudolph_icon),
+            name = "Rudolph",
+            bio = "Has a red nose."
+        )
     }
 }
