4dd4f087ab4ae54da070eb99e5ae330a86a1a3af
96SLM <slmitchell2@pstcc.edu>
2025-02-09 23:56:49 -0500

    Project now displays preview after  a copy of repository was made. do not open file within onedrive! Project not tested i android emulators, project preview displays project.

--- app/src/main/java/com/example/profilecard/MainActivity.kt
+++ app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -5,12 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.profilecard.ui.theme.ProfileCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,40 +32,45 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProfileCardTheme {
-//                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-//                    /*Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )*/
-//                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )
+                {
+                    ProfileBio(
+                        image = painterResource(id = R.drawable.rudolph_icon),
+                        name = "Rudolph",
+                        bio = "Has a red nose."
+                    )
+                }
             }
         }
     }
 }
-//Pick up form "Run your first app on the Android Emulator"
-/*@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}*/
-
-@Composable
-fun ProfileCardApp(){}
 
 @Composable
 fun ProfileBio(image: Painter, name: String, bio: String){
-    Column{
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(8.dp)
+    ){
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = "A photo of Rudolph from the old stop-motion films.",
+            modifier = Modifier.size(120.dp).clip(CircleShape)
         )
         Text(
-            text = name
+            text = name,
+            fontSize = 20.sp,
+            lineHeight = 15.sp,
+            modifier = Modifier.padding(3.dp)
+                .align(alignment = Alignment.Start)
         )
         Text(
-            text = bio
+            text = bio,
+            fontSize = 10.sp,
+            lineHeight = 5.sp,
+            textAlign = TextAlign.End
         )
     }
 }
@@ -61,7 +79,6 @@ fun ProfileBio(image: Painter, name: String, bio: String){
 @Composable
 fun ProfileCardPreview() {
     ProfileCardTheme {
-        ProfileCardApp()
         ProfileBio(
             image = painterResource(id = R.drawable.rudolph_icon),
             name = "Rudolph",
